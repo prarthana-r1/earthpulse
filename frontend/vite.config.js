e22d2b1e6a47d6723b7fd1d15cdd673f45872964
prarthana-r1 <prarthana.info.in@gmail.com>
2025-12-12 16:54:30 +0530

    sw

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,7 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   plugins: [
     react(),
@@ -6,7 +10,7 @@ export default defineConfig({
 
   build: {
     outDir: "dist",
-    copyPublicDir: true,   // ⬅ copies public/sw.js → dist/sw.js
+    copyPublicDir: true,   // IMPORTANT for sw.js
   },
 
   server: {
